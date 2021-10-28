3f35fe7486d678e3d81f05113eee09f6337a15a2
vxccx <86115107+vxccx@users.noreply.github.com>
2021-10-28 09:46:20 +0200

    update cesplib

--- src/libs/cesplib_rars.asm
+++ src/libs/cesplib_rars.asm
@@ -6,3 +6,18 @@
 # GAME SPECIFIC
 .eqv BORDER_SIZE 5
 .eqv CELL_SIZE 82
+
+.eqv CURSOR_SIZE 8
+
+
+j cesplib.end
+
+cesp_sleep:
+# Input:
+#   a0: number of ms to sleep
+  li a7, 32
+  ecall
+  ret
+
+
+cesplib.end:
